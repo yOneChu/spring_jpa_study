3983bb5871cc0e5f259b16bee73bb16e7bfc0225
kyhslam <kyhslam@gmail.com>
2020-07-13 18:30:21 +0900

    csv file

--- spring_jpa/src/test/java/org/kyhslam/thread/ThreadMain.java
+++ spring_jpa/src/test/java/org/kyhslam/thread/ThreadMain.java
@@ -4,13 +4,13 @@ public class ThreadMain {
 
 	public static void main(String[] args) {
 		
-		ThreadTest1 aa = new ThreadTest1();
+		//ThreadTest1 aa = new ThreadTest1();
 		ThreadTest2 bb = new ThreadTest2();
 		
-		Thread t1 = new Thread(aa, "company");
+		//Thread t1 = new Thread(aa, "company");
 		Thread t2 = new Thread(bb, "account");
 		
-		t1.start();
+		//t1.start();
 		t2.start();
 		
 		Thread.currentThread().getName();
--- spring_jpa/src/test/java/org/kyhslam/thread/ThreadTest1.java
+++ spring_jpa/src/test/java/org/kyhslam/thread/ThreadTest1.java
@@ -1,7 +1,26 @@
 package org.kyhslam.thread;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import com.opencsv.CSVReader;
+
+
+//ACCOUNT
 public class ThreadTest1 implements Runnable {
 
+	
+	private File fileDir;
+	private String type;
+	private String isDir;
+	
+	public ThreadTest1(String fileType, String isDir) {
+		this.type = fileType;
+		this.isDir = isDir;
+	}
 	
 	@Override
 	public void run() {
@@ -10,6 +29,46 @@ public class ThreadTest1 implements Runnable {
 			System.out.println("i == " + i);
 		}
 		
-	}
-	
+		try {
+			
+			for(File file : new File(isDir).listFiles()){
+				if(file.isFile()){
+					if(file.getName().contains(type)){
+						
+						ArrayList<String[]> list = new ArrayList<String[]>();
+						
+						BufferedReader bufferedReader = new BufferedReader(
+			                    new InputStreamReader(new FileInputStream(file.getAbsolutePath()), "euc-kr"));
+
+			            CSVReader reader = new CSVReader(bufferedReader);
+			            String[] nextLine;
+
+			            while( (nextLine = reader.readNext()) != null ){
+			            	String[] data = new String[nextLine.length];
+			                data[4] = nextLine[4];
+			                data[5] = nextLine[5];
+			                data[6] = nextLine[6];
+			                data[7] = nextLine[7];
+			                data[8] = nextLine[8];
+			                data[9] = nextLine[9];
+			                data[10] = nextLine[10];
+			                data[11] = nextLine[11]; // 도메인명
+			                
+			                list.add(data);
+			            }// end while
+			            
+			            
+			            
+			            
+			            
+					}
+				}
+				
+				
+			} // end for
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 }
